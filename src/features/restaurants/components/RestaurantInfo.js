8f3faebf3e4021bdc849bbc29e37767076a12bf5
Dakota Pfeifer <d-pfeif@outlook.com>
2023-10-01 16:18:21 -0700

    Spacer component

--- src/features/restaurants/components/RestaurantInfo.js
+++ src/features/restaurants/components/RestaurantInfo.js
@@ -5,6 +5,7 @@ import { Card } from "react-native-paper";
 import { SvgXml } from "react-native-svg";
 import star from "../../../../assets/star";
 import open from "../../../../assets/open";
+import Spacer from "../../../components/spacer/SpacerComponent";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -83,9 +84,9 @@ const RestaurantInfo = ({ restaurant = {} }) => {
                 CLOSED TEMPORARILY
               </Text>
             )}
-            <View style={{ paddingLeft: 16 }} />
+            <Spacer variant="left.large" />
             {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
-            <View style={{ paddingLeft: 16 }} />
+            <Spacer variant="left.large" />
             <Image style={{ width: 15, height: 15 }} source={{ uri: icon }} />
           </SectionEnd>
         </Section>
